d18fb47d8e8f2869fa984c7224a82ac16340343e
Sohail <msohail.dev08@gmail.com>
2026-02-24 01:19:37 +0530

    Add two class files

--- java-practice/statickeyword/A.java
+++ java-practice/statickeyword/A.java
@@ -1,1 +1,12 @@
+package com.practice.statickeyword;
 
+public class A {
+
+    int a;
+
+    public void method1(final int a){
+
+      //  a = 34;  // --> gives error "Cannot assign a value to final variable 'a' " because a is final.
+        System.out.println("Print " + a);
+    }
+}
---
+++ java-practice/statickeyword/B.java
@@ -0,0 +1,11 @@
+package com.practice.statickeyword;
+
+public class B {
+
+    public static void main(String[] args) {
+
+        A a = new A();
+        a.method1(35);           // prints this input value , this value is final in method1
+
+    }
+}
